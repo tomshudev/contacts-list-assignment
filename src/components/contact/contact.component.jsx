02358e8769dfb7776656c8cd2d2ba2c494763e79
tomshudev <tomshudev@gmail.com>
2020-11-17 17:46:30 +0200

    Styling the contact

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import './contact.styles.scss';
+import "./contact.styles.scss";
 
 function Contact({ contact }) {
-    return (
-        <h1>{contact.name}</h1>
-    )
+  return (
+    <div className="contact">
+      <img className="image" src={contact.profile_image} alt={contact.name} />
+      <img
+        className="icon"
+        src={contact.icon}
+        alt={`${contact.name}-icon    `}
+      />
+
+      <div className="information">
+        <div className="name">{contact.name}</div>
+        <div className="job">{`${contact.job} | ${contact.company_name}`}</div>
+      </div>
+
+      <br />
+      <br />
+      <div className="hidden-info">
+        <div className="phone">Phone number: {contact.phone}</div>
+        <div className="email">{contact.email}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
