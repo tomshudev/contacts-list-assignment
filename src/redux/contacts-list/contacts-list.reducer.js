38f86fe89bd851ecd38a3b6e1aa08d280736ceee
tomshudev <tomshudev@gmail.com>
2020-11-18 23:54:43 +0200

    Initialize the contacts list with undefined

--- src/redux/contacts-list/contacts-list.reducer.js
+++ src/redux/contacts-list/contacts-list.reducer.js
@@ -1,29 +1,29 @@
-import {ContactsListActionTypes} from './contacts-list.actions';
+import { ContactsListActionTypes } from "./contacts-list.actions";
 
 const initialState = {
-    searchValue: '',
-    contacts: []
-}
+  searchValue: "",
+  contacts: undefined,
+};
 
 const contactsListReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case (ContactsListActionTypes.SEARCH_VALUE_CHANGED): {
-            return {
-                ...state,
-                searchValue: action.payload
-            }
-        }
-
-        case (ContactsListActionTypes.SET_CONTACTS_LIST): {
-            return {
-                ...state,
-                contacts: action.payload
-            }
-        }
-
-        default:
-            return state;
+  switch (action.type) {
+    case ContactsListActionTypes.SEARCH_VALUE_CHANGED: {
+      return {
+        ...state,
+        searchValue: action.payload,
+      };
     }
-}
+
+    case ContactsListActionTypes.SET_CONTACTS_LIST: {
+      return {
+        ...state,
+        contacts: action.payload,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
 
 export default contactsListReducer;
